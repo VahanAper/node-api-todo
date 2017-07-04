1f121c24005d717f91db8a51b5de7f3c4d3a0ec2
VahanAper <asryanvahandev@gmail.com>
2017-07-04 16:55:43 +0400

    refactor mongoose models files

--- server/server.js
+++ server/server.js
@@ -1,32 +1,4 @@
-const mongoose = require('mongoose');
+const { mongoose } = require('./db/mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-const Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlenght: 1,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null,
-  },
-});
-
-const newTodo = new Todo({
-  text: 'Make a video.'
-});
-
-newTodo.save()
-  .then((doc) => {
-    console.log('Saved Todo.', doc);
-  }, e => {
-    console.log('Unable to save Todo.', e);
-  });
+const { Todo } = require('./models/Todo');
+const { User } = require('./models/User');
