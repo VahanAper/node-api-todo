8234424c7797ca135618ead9d66d2164c4037614
VahanAper <asryanvahandev@gmail.com>
2017-07-05 13:06:01 +0400

    add test from GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const supertest = require('supertest');
 const { app } = require('../server');
 const { Todo } = require('../models/Todo');
 
+const todos = [
+  { text: 'First test todo' },
+  { text: 'Second test todo' },
+];
+
 beforeEach((done) => {
   Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
     .then(() => done());
 });
 
@@ -25,7 +33,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find()
+        Todo.find({ text })
           .then((todos) => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -49,7 +57,7 @@ describe('POST /todos', () => {
 
         Todo.find()
           .then((todos) => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch((e) => {
@@ -57,4 +65,16 @@ describe('POST /todos', () => {
           });
       });
   })
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    supertest(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
