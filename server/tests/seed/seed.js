96525a1cab893177db40096ed9d2cf735b9837dc
VahanAper <asryanvahandev@gmail.com>
2017-07-10 15:01:48 +0400

    refactor test files for seed

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,29 @@
+const { ObjectID } = require('mongodb');
+
+const { Todo } = require('../../models/Todo');
+
+const todos = [
+  {
+    _id: new ObjectID(),
+    text: 'First test todo',
+  },
+  {
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333,
+  },
+];
+
+const populateTodos = (done) => {
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => done());
+};
+
+module.exports = {
+  todos,
+  populateTodos,
+};
